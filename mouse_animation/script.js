cc5d927075fa88cecdcbca2d31429261d776637e
matteoorig <matteoorig@gmail.com>
2022-03-13 12:19:13 +0100

    update

--- mouse_animation/script.js
+++ mouse_animation/script.js
@@ -34,65 +34,106 @@ const getSiblings = (e) => {
     return siblings;
 };
 
+//Cursor
+let mouse = {
+  x:0,
+  y:0
+};
 
+window.addEventListener("mousemove", (ev) =>(mouse = getMousePos(ev)));
 
-//main
-import gsap from "gsap";
-let mouse = {x: 0, y: 0};
-window.addEventListener("mousemove", (ev) => (mouse = getMousePos(ev)));
-class Cursor {
-  constructor(el) {
-    // Varibles
-    this.Cursor = el;
-    this.Cursor.style.opacity = 0;
-    this.Item = document.querySelectorAll(".hero-inner-link-item");
-    this.Hero = document.querySelector(".hero-inner");
-    this.bounds = this.Cursor.getBoundingClientRect();
-    this.cursorConfigs = {
-      x: { previous: 0, current: 0, amt: 0.2 },
-      y: { previous: 0, current: 0, amt: 0.2 },
-    };
-    this.onMouseMoveEv = () => {
-      this.cursorConfigs.x.previous = this.cursorConfigs.x.current = mouse.x;
-      this.cursorConfigs.y.previous = this.cursorConfigs.y.previous = mouse.y;
+class Cursor{
+  constructor(el){
+      this.Cursor = el;
+      this.Cursor.style.opacity = 0;
+      this.Item = document.querySelectorAll("h1");
+      this.Hero = document.querySelector(".container");
+      this.bounds = this.Cursor.getBoundingClientRect();
+      this.cursorConfigs = {
+        x: {previous: 0, current: 0, amt: 0.2},
+        y: {previous: 0, current: 0, amt: 0.2},
+      }
+      this.onMouseMoveEv = () =>{
+        this.cursorConfigs.x.previous = this.cursorConfigs.x.current = mouse.x;
+        this.cursorConfigs.y.previous = this.cursorConfigs.y.current = mouse.y;
 
-      // Set cursor opacity to 1 when hovered on screen
-      gsap.to(this.Cursor, {
-        duration: 1,
-        ease: "Power3.easeOut",
-        opacity: 1,
-      });
-      // Execute scale function
-      this.onScaleMouse();
+        //animation
+        gsap.to(this.Cursor, {
+          duration:1,
+          ease: "Power3.easeOut",
+          opacity:1,
+        })
 
-      // The window.requestAnimationFrame() method tells the browser that you wish to perform an animation and requests that the browser calls a specified function to update an animation before the next repaint. The method takes a callback as an argument to be invoked before the repaint.
-      requestAnimationFrame(() => this.render());
-      // Clean up function
-      window.removeEventListener("mousemove", this.onMouseMoveEv);
-    };
-    // Scale cursor animation
-    window.addEventListener("mousemove", this.onMouseMoveEv);
+        this.onScaleMouse();
+        requestAnimationFrame(()=> this.render());
+
+        //funzione di clean-up
+        window.removeEventListener("mousemove", this.onMouseMoveEv)
+      }
+
+      //assegno eventListener personalizzato al mouse
+      window.addEventListener("mousemove", this.onMouseMoveEv);
   }
-  render() {
+
+  onScaleMouse() {
+    // Loop through all items
+    this.Item.forEach((link) => {
+      
+      //event in entrata nel container del titolo
+      link.addEventListener("mouseenter", () => {
+        this.Cursor.classList.add("media-blend");
+        this.scaleAnimation(this.Cursor.children[0], 0.8, 0.6);
+      });
+
+      //event in uscita dal container del titolo
+      link.addEventListener("mouseleave", () => {
+        this.Cursor.classList.remove("media-blend");
+        this.scaleAnimation(this.Cursor.children[0], 0, 0.8);
+      });
+
+      
+
+    
+
+      
+
+
+    });
+   }
+
+   scaleAnimation(el, amt, duration){
+    gsap.to(el, {
+      duration:duration,
+      scale: amt,
+      ease: "Power3.easeOut",
+    })
+   }
+
+
+
+
+
+  render(){
     this.cursorConfigs.x.current = mouse.x;
     this.cursorConfigs.y.current = mouse.y;
 
-    // lerp
-    for (const key in this.cursorConfigs) {
-      // key will be x & y
-      // WTF IS LERP?
-      // Lerp - A lerp returns the value between two numbers at a specified, decimal midpoint:
-      this.cursorConfigs[key].previous = lerp(
-        this.cursorConfigs[key].previous,
-        this.cursorConfigs[key].current,
-        this.cursorConfigs[key].amt
+    for(const k in this.cursorConfigs){
+      this.cursorConfigs[k].previous = lerp(
+        this.cursorConfigs[k].previous,
+        this.cursorConfigs[k].current,
+        this.cursorConfigs[k].amt
       );
     }
-    // Setting the cursor x and y to our cursoer html element
+
+    //render in html
     this.Cursor.style.transform = `translateX(${this.cursorConfigs.x.previous}px) translateY(${this.cursorConfigs.y.previous}px)`;
-    // RAF
-    requestAnimationFrame(() => this.render());
+
+    requestAnimationFrame(()=> this.render());
   }
 }
 
+//main
 
+window.onload = () =>{
+  const cursor = new Cursor(document.querySelector(".cursor"));
+}
