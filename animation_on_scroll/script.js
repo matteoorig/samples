df740d0ecb673ab1cc01a2ccaa5b80b9d4b7e32c
matteoorig <matteoorig@gmail.com>
2022-03-14 11:36:35 +0100

    update

--- animation_on_scroll/script.js
+++ animation_on_scroll/script.js
@@ -1,15 +1,18 @@
 var tl = new TimelineMax({onUpdate: updatePercentage});
 const controller = new ScrollMagic.Controller();
 
+const container = document.querySelector(".container");
+
 tl.from(".item", .5, {x: 200, opacity: 0})
 
 const scene = new ScrollMagic.Scene({
     triggerElement: ".container",
     triggerHook: "onLeave",
-    duration: "25%"
+    duration: "100%"
 }).setPin(".container").setTween(tl).addTo(controller);
 
 function updatePercentage(){
     tl.progress();
-    console.log(tl.progress());
+    console.log( window.innerHeight);
+    
 }
